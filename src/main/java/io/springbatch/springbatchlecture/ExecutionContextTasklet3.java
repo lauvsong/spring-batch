a4b0d914dd4e6e9375fcaf70d1d1994937d69df8
김혜송 <hyehye9355@naver.com>
2022-10-17 17:28:53 +0900

    :sparkles: execution context 공유 여부 테스트 코드 추가

--- src/main/java/io/springbatch/springbatchlecture/ExecutionContextTasklet3.java
+++ src/main/java/io/springbatch/springbatchlecture/ExecutionContextTasklet3.java
@@ -12,6 +12,13 @@ public class ExecutionContextTasklet3 implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
         System.out.println("step3 was executed");
+        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
+
+        if (name == null) {
+            chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().put("name", "user1");
+            throw new RuntimeException("step3 was failed");
+        }
+
         return RepeatStatus.FINISHED;
     }
 }
--- src/main/java/io/springbatch/springbatchlecture/ExecutionContextTasklet4.java
+++ src/main/java/io/springbatch/springbatchlecture/ExecutionContextTasklet4.java
@@ -11,6 +11,7 @@ public class ExecutionContextTasklet4 implements Tasklet {
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("name: " + chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name"));
         System.out.println("step4 was executed");
         return RepeatStatus.FINISHED;
     }
