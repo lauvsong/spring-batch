c683765a1fc276d6fe5c4809ff69edb1d5c6e975
lauvsong <hyehye9355@naver.com>
2022-10-31 10:43:13 +0900

    :wrench: batch 제시작 테스트용 configuration 추가

---
+++ src/main/java/io/springbatch/springbatchlecture/PreventRestartConfiguration.java
@@ -0,0 +1,49 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class PreventRestartConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job preventRestartJob() {
+        return jobBuilderFactory.get("preventRestartJob")
+                .start(step1())
+                .next(step2())
+                .preventRestart()
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    throw new RuntimeException("step2 was failed");
+//                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
