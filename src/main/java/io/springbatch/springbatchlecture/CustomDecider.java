8b3c31624147ae62db3fdeabbb43e8aafd3de5f3
lauvsong <hyehye9355@naver.com>
2022-12-08 16:38:44 +0900

    :sparkles: decider 동작 테스트용 객체 구현

---
+++ src/main/java/io/springbatch/springbatchlecture/CustomDecider.java
@@ -0,0 +1,25 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+public class CustomDecider implements JobExecutionDecider {
+
+    private int count = 0;
+
+    @Override
+    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+
+        count++;
+
+        if ((count & 1) == 0) {
+            return new FlowExecutionStatus("EVEN");
+        } else {
+            return new FlowExecutionStatus("ODD");
+        }
+
+    }
+
+}
