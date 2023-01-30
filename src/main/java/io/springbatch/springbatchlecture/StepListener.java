7a7dd93cf667778a70909a617e1f3408859808e8
lauvsong <hyehye9355@naver.com>
2023-01-30 10:54:04 +0900

    :sparkles: Scope 테스트 용 StepListener 생성

---
+++ src/main/java/io/springbatch/springbatchlecture/StepListener.java
@@ -0,0 +1,18 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+
+public class StepListener implements StepExecutionListener {
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        stepExecution.getExecutionContext().putString("name2", "user2");
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        return null;
+    }
+}
