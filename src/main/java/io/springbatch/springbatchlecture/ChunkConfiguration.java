04c81a68bce3803a14d985162874dcc49cf4cbfc
lauvsong <hyehye9355@naver.com>
2023-01-30 11:35:49 +0900

    :wrench: Chunk 테스트용 Job 생성

---
+++ src/main/java/io/springbatch/springbatchlecture/ChunkConfiguration.java
@@ -0,0 +1,54 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ChunkConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkBasedJob() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("item = " + item);
+                        return "my" + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("items = " + items);
+                    }
+                })
+                .build();
+    }
+
+}
